09d78e47d9da1387569e965749a98e6f850e2269
fatima <omfatimamod@gmail.com>
2022-02-27 14:23:52 -0600

    Added new GitHubChanges class with some useful method

---
+++ src/test/java/steps/GitHubChanges.java
@@ -0,0 +1,14 @@
+package steps;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class GitHubChanges {
+
+    public static void main(String[] args) {
+
+
+        String name = "Fatima";
+        System.out.println(name);
+    }
+}
+
